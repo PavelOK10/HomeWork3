20e0653f2a213833f423d7f1c913e62006730434
PaRus <PaRusakovich@gmail.com>
2021-04-28 12:51:46 +0300

    Добавьте файлы проекта.

---
+++ Calculator-Switch/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double a, c;
+
+                try
+                {
+                    Console.WriteLine("Введите первое число");
+                    a = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число");
+                    c = Convert.ToDouble(Console.ReadLine());
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Неправильный ввод данных. Попробуйте еще раз");
+                    Console.ReadLine();
+                    continue;
+                }
+                    Console.WriteLine("Выберите знак операции '+', '-', '*', '/' ");
+                    string b = Console.ReadLine();
+
+                switch (b)
+                {
+                    case "+":
+                        Console.WriteLine("Операция сложения равна: " + (a + c));
+                        break;
+
+                    case "-":
+                        Console.WriteLine("Операция вычитания равна: " + (a - c));
+                        break;
+
+                    case "*":
+                        Console.WriteLine("Операция умножения равна: " + (a * c));
+                        break;
+
+                    case "/":
+                        if (c == 0)
+                            Console.WriteLine("Делить на ноль нельзя");
+                        else
+                            Console.WriteLine("Операция деления равна: " + (a / c));
+                        break;
+
+                    default:
+                        Console.WriteLine("Неизвестное действие");
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ HomeWork3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double a, c;
+                string b;
+
+                try
+                {
+                    Console.WriteLine("Введите первое число");
+                    a = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число");
+                    c = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Неправильный ввод данных. Попробуйте еще раз");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                    Console.WriteLine("Выберите знак операции '+', '-', '*', '/' ");
+                    b = Console.ReadLine();
+
+                if (b == "+")
+                {
+                    Console.WriteLine("Операция сложения равна: " + (a + c));
+                }
+                else if (b == "-")
+                {
+                    Console.WriteLine("Операция вычитания равна: " + (a - c));
+                }
+
+                else if (b == "*")
+                {
+                    Console.WriteLine("Операция умножения равна: " + (a * c));
+                }
+                else if (b == "/")
+                {
+                    if (c == 0)
+                        Console.WriteLine("Делить на ноль нельзя");
+                    else
+                        Console.WriteLine("Операция деления равна: " + (a / c));
+
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
